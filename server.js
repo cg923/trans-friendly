9c2974e30eeb9f4819d33bb5791d0f350050be29
cg923 <coreygselover@gmail.com>
2017-08-02 13:59:15 -0600

    More files and basic express code

--- server.js
+++ server.js
@@ -7,3 +7,20 @@ const morgan		= require('morgan');
 const cookieParser	= require('cookie-parser');
 const bodyParser	= require('body-parser');
 const session		= require('express-session');
+
+mongoose.connect('mongod://localhost/transplaces');
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('views', './views');
+app.engine('ejs', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+app.use(session({secret: 'Corey-doesnt-know-what-this-does'}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
