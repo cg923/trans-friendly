7a66d94b6f75569b109b40ebcb1ec26cd3418b37
cg923 <coreygselover@gmail.com>
2017-08-02 14:10:55 -0600

    Adds basic routes

---
+++ config/routes.js
@@ -0,0 +1,24 @@
+const express 			= require('express');
+const router 			= express.Router();
+const bodyParser 		= require('body-parser');
+const methodOverride 	= require('method-override');
+const passport			= require('passport');
+const usersController	= require('../controllers/users');
+const placesController	= require('../controllers/places');
+const staticsController	= require('../controllers/statics');
+
+router.route('/')
+	.get(staticsController.home);
+
+router.route('/signup')
+	.get(usersController.getSignup)
+	.post(usersController.postSignup);
+
+router.route('/login')
+	.get(usersController.getLogin)
+	.post(usersController.postLogin);
+
+router.route('/logout')
+	.get(usersController.getLogout);
+
+module.exports = router;
