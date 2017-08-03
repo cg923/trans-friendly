134b59522a1935767ce35e678bcc8b2ee6a83cb4
cg923 <coreygselover@gmail.com>
2017-08-03 11:25:12 -0600

    InfoWindows appear when user clicks map marker

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,6 @@
 var map;
 var openInfoWindows = [];
 var markers = [];
-var radius;
 
 $(document).ready(function() {
 	$('#search-form').submit(function(event) {
@@ -10,82 +9,106 @@ $(document).ready(function() {
 	});
 });
 
+function createInfoWindow(place, callback) {
+	//Check our DB to see if we have data on the place.
+	var reviews = [];
+	var friendliness;
+	var friendImgSrc = '';
+	var genNeutBath = '/img/check_false.png';
+	var lgbtOwned = '/img/check_false.png';
+	var advertises = '/img/check_false.png';
+
+	var infoWindow = new google.maps.InfoWindow({
+		content: "Hi!"
+	});
+
+	openInfoWindows.push(infoWindow);
+	callback(infoWindow, null);
+}
+
+function closeOpenInfoWindows() {
+	// Hide reviews
+	//$('#reviews').addClass('hidden');
+
+	// Close info windows.
+	if (openInfoWindows.length === 0) return;
+
+	openInfoWindows.forEach(function(element) {
+		element.close();
+	});
+}
+
+function calculateSearchRadius() {
+	// Calculate an appropriate search radius.
+  	// TO DO - This should be an equation.
+	switch (map.getZoom()) {
+	    case 20:
+		    return 50;
+	    case 19:
+		    return 75;
+	    case 18:
+		    return 100;
+	    case 17:
+		    return 250;
+	    case 16:
+		    return 500;
+	    case 15:
+		    return 750;
+	    case 14:
+		    return 3250;
+	    case 13:
+		    return 5500;
+	    case 12:
+		    return 7750;
+	    default:
+		    return 10000;
+	}
+}
+
 function populateMap(searchTerm, location) {
-  // Calculate an appropriate search radius.
-  // TO DO - This should be an equation.
-  switch (map.getZoom()) {
-    case 20:
-      radius = 50;
-      break;
-    case 19:
-      radius = 75;
-      break;
-    case 18:
-      radius = 100;
-      break;
-    case 17:
-      radius = 250;
-      break;
-    case 16:
-      radius = 500;
-      break;
-    case 15:
-      radius = 750;
-      break;
-    case 14:
-      radius = 3250;
-      break;
-    case 13:
-      radius = 5500;
-      break;
-    case 12:
-      radius = 7750;
-      break;
-    default:
-      radius = 10000;
-      break;
-  }
+  
+    var radius = calculateSearchRadius();
 
-  $.ajax({
-    url: '/api/google',
-    method: "POST",
-    data: { 
-      search: searchTerm, 
-      location: {
-        lat: location.lat(),
-        lng: location.lng()
-      },
-      radius: radius
-    },
-    success: function(results) {
-      
-      // Remove active markers from the map.
-      markers.forEach(function(element) {
-        element.setMap(null);
-      });
-      markers = [];
+    $.ajax({
+	    url: '/api/google',
+	    method: "POST",
+	    data: { 
+	        search: searchTerm, 
+	        location: {
+	        	lat: location.lat(),
+	        	lng: location.lng()
+	        },
+	      	radius: radius
+    	},
+	    success: function(results) {
+	      
+	        // Remove active markers from the map.
+	        markers.forEach(function(element) {
+	        	element.setMap(null);
+	        });
+	        markers = [];
 
-      // Create markers for new results.
-      results.forEach(function(element) {
-        // Create corresponding infoWindows (Google's little popup bubbles)
-        //createInfoWindow(element, function(infoWindow, results) {
-          var marker = new google.maps.Marker({
-            position: {lat: element.geometry.location.lat, lng: element.geometry.location.lng},
-            map: map,
-            title: element.name
-          });
-          markers.push(marker);
+	        // Create markers for new results.
+	        results.forEach(function(element) {
+	        	// Create corresponding infoWindows (Google's little popup bubbles)
+	        	createInfoWindow(element, function(infoWindow, results) {
+		            var marker = new google.maps.Marker({
+			            position: {lat: element.geometry.location.lat, lng: element.geometry.location.lng},
+			            map: map,
+			            title: element.name
+			        });
+		            markers.push(marker);
 
-          // When user clicks on a marker, display the corresponding infoWindow.
-          marker.addListener('click', function() {
-            closeOpenInfoWindows();
-            populateReviewList(results);
-            infoWindow.open(map, marker);
-          });
-        //});
-      });
-    }
-  });
+		            // When user clicks on a marker, display the corresponding infoWindow.
+		            marker.addListener('click', function() {
+		            	closeOpenInfoWindows();
+		            	//populateReviewList(results);
+		            	infoWindow.open(map, marker);
+		            });
+	        	});
+	    	});
+    	}
+  	});
 }
 
 // Sets up map on the screen.
