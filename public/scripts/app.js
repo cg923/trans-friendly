3887bcdda464ecbd8b02cd5ea3588819d3d3b21c
cg923 <coreygselover@gmail.com>
2017-08-03 10:32:13 -0600

    Search form displays on page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,8 +1,21 @@
 var map;
 
+// Sets up map on the screen.
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
 	    zoom: 15,
+	    // Denver is the default location :)
 	    center: {lat: 39.7392358, lng: -104.990251}
 	});
+
+	// Try to get user's location.
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			map.setCenter(pos);
+		});
+	}
 }
